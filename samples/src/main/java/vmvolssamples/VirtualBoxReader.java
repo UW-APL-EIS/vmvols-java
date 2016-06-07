5886c5dd77f64c538cb026073a990429643747f1
Stuart Maclean <stuart@apl.uw.edu>
2016-06-06 21:25:06 -0700

    Add license headers

--- samples/src/main/java/vmvolssamples/VirtualBoxReader.java
+++ samples/src/main/java/vmvolssamples/VirtualBoxReader.java
@@ -1,3 +1,30 @@
+/**
+ * Copyright © 2015, University of Washington
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Washington nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY
+ * OF WASHINGTON BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package vmvolssamples;
 
 import java.io.File;
@@ -22,8 +49,8 @@ import edu.uw.apl.vmvols.model.virtualbox.VBoxVM;
  *
  * The latter is used in our fuse-enabling class
  * edu.uw.apl.vmvols.fuse.VirtualMachineFilesystem.  We let
- * Fuse4j/fuse calculate the seek positionsand byte read counts, we
- * just provide access to the data.
+ * Fuse4j/fuse calculate the seek positions and byte read counts, we
+ * just provide access to the underlying data.
  *
  */
 public class VirtualBoxReader {
